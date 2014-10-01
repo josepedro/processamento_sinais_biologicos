921de96805a85a67fa92216b95744cf2d7a87c67
CristianoJacques <github.20.culture@antichef.net>
2014-10-01 13:34:04 -0300

    Incluída a função check_perfect_reconstruction_conditions_qmf
    Incluída a função check_perfect_reconstruction_conditions_qmf, que testa
    os filtros de decomposição e reconstrução quanto às condições para
    reconstrução perfeita.

--- resposta_freq.m
+++ resposta_freq.m
@@ -1,7 +1,9 @@
-function [H,f]=resposta_freq(h,N)
+function [H, f] = resposta_freq(h,N);
+
 	if nargin < 2
 		N = 1e6;
 	end 
+
 	H = fft(h,N);
-	H=fftshift(H);
-	f = linspace(-.5,.5,N);
+	H = fftshift(H);
+	f = linspace(-.5, .5, N);
