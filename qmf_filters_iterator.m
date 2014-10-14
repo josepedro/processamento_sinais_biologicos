0f0ef5cb195e6398f32fa01b9d5dc96f4b571120
Jose Pedro <1jpsneto@gmail.com>
2014-10-13 21:36:45 -0300

    Adding qmf_filters_iterator.m

---
+++ qmf_filters_iterator.m
@@ -0,0 +1,11 @@
+function h = qmf_filters_iterator...
+             (h0, h1, number_levels);
+
+h{number_levels + 1} = h1;
+h0iterated = h0;
+for (k = number_levels : -1 : 2)
+	h{k} = conv(h0, upsample(h{k + 1}, 2));
+	h0iterated = conv(h0,...
+	             upsample(h0iterated, 2));
+end
+h{1} = h0iterated;
